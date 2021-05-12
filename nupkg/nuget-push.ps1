8bbe61591e90294b8845080a6f0b9d78497c4f97
ivan <ivan@boxfusion.co.za>
2021-05-12 14:42:24 +0300

    fixed nuget-push.ps1, added nuget folder

--- nupkg/nuget-push.ps1
+++ nupkg/nuget-push.ps1
@@ -1,9 +1,9 @@
-$key = $env:private_nuget_apikey
+$key = $env:boxfusion_nuget_apikey
 if ($key) {
 	Write-Output "API KEY found"
 	$packages = Get-ChildItem -Recurse packages\*.nupkg
 	foreach ($pkg in $packages) {
-		..\.nuget\nuget push $pkg.FullName -source https://nuget.boxfusion.co.za/api/v2/package -apikey $key
+		..\.nuget\nuget push $pkg.FullName -source https://nuget.org/api/v2/package -apikey $key
 	}
 } else {
 	Write-Output "API KEY not found!"
