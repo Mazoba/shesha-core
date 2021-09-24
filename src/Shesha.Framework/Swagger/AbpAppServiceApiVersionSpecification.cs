078f40b47deed2327994c6ae67fe3c66176f5252
ivan <ivan@boxfusion.co.za>
2021-09-24 12:46:16 +0300

    added some swagger configuration classes

---
+++ src/Shesha.Framework/Swagger/AbpAppServiceApiVersionSpecification.cs
@@ -0,0 +1,35 @@
+﻿using Abp.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shesha.Swagger
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AbpAppServiceApiVersionSpecification : IApiControllerSpecification
+    {
+        public bool IsSatisfiedBy(ControllerModel controller)
+        {
+            var typeInfo = controller.ControllerType;
+            var type = controller.ControllerType.AsType();
+            if (!typeof(IApplicationService).IsAssignableFrom(type) ||
+               !typeInfo.IsPublic || typeInfo.IsAbstract || typeInfo.IsGenericType)
+            {
+                return false;
+            }
+            if (!(controller.Attributes is List<object> attributes))
+            {
+                return false;
+            }
+            if (typeInfo.IsDefined(typeof(ApiVersionAttribute), false))
+            {
+                attributes.Add(new ApiVersionAttribute("1")); //Set default version
+            }
+            return true;
+        }
+    }
+}
