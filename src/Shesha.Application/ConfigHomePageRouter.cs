55a2851084c25c7ccbf00c2647cf331ab6e42c49
Ian Houvet <ian.houvet@boxfusion.io>
2022-06-08 23:41:21 +0200

    Implemented ability to specify/Configure HomeUrl

---
+++ src/Shesha.Application/ConfigHomePageRouter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Shesha.Authorization.Users;
+using Shesha.Services;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shesha
+{
+    /// <summary>
+    /// Specifies the page a user should be directed
+    /// to by default straight after successfully loging in based on the AppSettings
+    /// </summary>
+    public class ConfigHomePageRouter : IHomePageRouter
+    {
+        //private readonly IConfiguration _config;
+
+        //public ConfigHomePageRouter()
+        //{
+        //    _config = config;
+        //}
+
+        public async Task<string> GetHomePageUrlAsync(User user)
+        {
+            var config = StaticContext.IocManager.Resolve<IConfiguration>();
+
+            var homeUrl = config["SheshaApp:HomeUrl"];
+
+            return homeUrl ?? string.Empty;
+        }
+
+    }
+}
---
+++ src/Shesha.Application/IHomePageRouter.cs
@@ -0,0 +1,20 @@
+﻿using Shesha.Authorization.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shesha
+{
+    /// <summary>
+    /// Interface for a class used to determine which page a user should be directed
+    /// to by default straight after successfully loging in.
+    /// </summary>
+    public interface IHomePageRouter
+    {
+
+        Task<string> GetHomePageUrlAsync(User user);
+
+    }
+}
