7da7ea07b4c643d6d6d2de4346cad3412c82e83b
Ian Houvet <ian.houvet@boxfusion.io>
2022-06-14 21:30:39 +0200

    Moved FrontEndApp and added DB Migration

--- src/Shesha.Framework/Domain/FrontEndApp.cs
+++ src/Shesha.Framework/Domain/FrontEndApp.cs
@@ -1,24 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Shesha.FrontEndApps
+namespace Shesha.Domain
 {
     /// <summary>
     /// A Front-end App represents the various front-end applications that build off this back-end. 
     /// For example, Admin Portal, Customer Portal, Customer Mobile App would be fairly typical examples.
     /// </summary>
-    public class FrontEndApp
+    public class FrontEndApp : FullPowerEntity
     {
         /// <summary>
-        /// Name of the front-end app
+        /// Name of the front-end app.
         /// </summary>
+        [StringLength(100)]
         public virtual string Name { get; set; }
 
         /// <summary>
-        /// 
+        /// Description of the Front-end application.
         /// </summary>
         public virtual string Description { get; set; }
 
