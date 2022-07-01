bcbe54ee5595551bf879660260a50174d11779f0
ivan <ivan@boxfusion.co.za>
2022-07-01 17:44:44 +0300

    fixed size of the AbpAuditLogs.ExceptionMessage

---
+++ src/Shesha.NHibernate/Migrations/Abp/M20220701174000.cs
@@ -0,0 +1,14 @@
+﻿using FluentMigrator;
+using Shesha.FluentMigrator;
+
+namespace Shesha.Migrations.Abp
+{
+    [Migration(2022070117400)]
+    public class M20220701174000 : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Alter.Table("AbpAuditLogs").AlterColumn("ExceptionMessage").AsStringMax().Nullable();
+        }
+    }
+}
