7c50b906ba9f9440a3545ba63284bf34fce6eda1
ivan <ivan@boxfusion.co.za>
2022-07-05 14:00:55 +0300

    GraphQL: entity schemas moved to cache with auto cleaning

--- src/Shesha.GraphQL/GraphQL/Provider/SchemaContainer.cs
+++ src/Shesha.GraphQL/GraphQL/Provider/SchemaContainer.cs
@@ -1,7 +1,11 @@
 ﻿using Abp.Dependency;
+using Abp.Events.Bus.Entities;
+using Abp.Events.Bus.Handlers;
 using Abp.Extensions;
 using Abp.Reflection;
+using Abp.Runtime.Caching;
 using GraphQL.Types;
+using Shesha.Domain;
 using Shesha.Extensions;
 using Shesha.GraphQL.Provider.Schemas;
 using System;
@@ -11,60 +15,103 @@ using System.Threading.Tasks;
 
 namespace Shesha.GraphQL.Provider
 {
-    public class SchemaContainer : ISchemaContainer, ISingletonDependency
+    public class SchemaContainer : ISchemaContainer, ISingletonDependency, IEventHandler<EntityChangedEventData<EntityProperty>>
     {
-        protected ISchema DefaultSchema { get; set; }
-
-        protected Dictionary<string, ISchema> Schemas { get; set; }
-
+        private readonly ICacheManager _cacheManager;
         private readonly ITypeFinder _typeFinder;
+        private readonly IServiceProvider _serviceProvider;
+
+        protected ISchema DefaultSchema { get; set; }
+        /// <summary>
+        /// Custom schemas
+        /// </summary>
+        protected Dictionary<string, ISchema> CustomSchemas { get; set; }
+
+        protected ITypedCache<string, ISchema> EntitySchemasCache
+        {
+            get
+            {
+                return _cacheManager.GetCache<string, ISchema>($"{this.GetType().Name}.{nameof(EntitySchemasCache)}");
+            }
+        }
 
         public SchemaContainer(
             //IOptions<AbpGraphQLOptions> options,
             IServiceProvider serviceProvider,
             IEnumerable<ISchema> customSchemas,
-            ITypeFinder typeFinder)
+            ITypeFinder typeFinder,
+            ICacheManager cacheManager)
         {
             DefaultSchema = new Schema();
             DefaultSchema.Query = new EmptyQuery();
             _typeFinder = typeFinder;
+            _cacheManager = cacheManager;
+            _serviceProvider = serviceProvider;
 
-            Schemas = customSchemas.ToDictionary(
+            CustomSchemas = customSchemas.ToDictionary(
                 keySelector: schema => schema.GetType().Name.RemovePostFix("Schema"),
                 elementSelector: schema => schema);
-
-            // find all entities and register schemas and queries
-            var entityTypes = _typeFinder.Find(t => t.IsEntityType()).ToList();
-            foreach (var entityType in entityTypes) 
-            {
-                var idType = entityType.GetEntityIdType();
-                var schemaType = typeof(EntitySchema<,>).MakeGenericType(entityType, idType);
-
-                var schema = Activator.CreateInstance(schemaType, serviceProvider);
-
-                var schemaName = entityType.Name.ToCamelCase();
-                Schemas.Add(schemaName, (ISchema)schema);
-            }
-
-            /*
-            foreach (var configuration in options.Value.AppServiceSchemes.GetConfigurations())
-            {
-                var schemaType = typeof(AppServiceSchema<,,,,>).MakeGenericType(configuration.AppServiceInterfaceType,
-                    configuration.GetOutputDtoType, configuration.GetListOutputDtoType, configuration.KeyType,
-                    configuration.GetListInputType);
-
-                var schema = Activator.CreateInstance(schemaType, serviceProvider);
-
-                Schemas.Add(configuration.SchemaName, (ISchema)schema);
-            }
-            */
         }
 
-        public virtual Task<ISchema> GetOrDefaultAsync(string schemaName, string specifiedDefaultSchemaName = null)
+        private ISchema GetEntitySchema(string schemaName)
         {
-            return Task.FromResult(!schemaName.IsNullOrEmpty() && Schemas.ContainsKey(schemaName)
-                ? Schemas[schemaName]
-                : specifiedDefaultSchemaName != null ? Schemas[specifiedDefaultSchemaName] : DefaultSchema);
+            var entityType = _typeFinder.Find(t => GetEntitySchemaName(t) == schemaName && t.IsEntityType()).FirstOrDefault();
+
+            return EntitySchemasCache.Get(schemaName, sn => GetEntitySchema(entityType, _serviceProvider));
+        }
+
+        private static ISchema GetEntitySchema(Type entityType, IServiceProvider serviceProvider) 
+        {
+            var idType = entityType.GetEntityIdType();
+            var schemaType = typeof(EntitySchema<,>).MakeGenericType(entityType, idType);
+
+            var schema = Activator.CreateInstance(schemaType, serviceProvider);
+
+            return (ISchema)schema;
+        }
+
+        /// <summary>
+        /// Get schema name for specified <paramref name="entityType"/>
+        /// </summary>
+        /// <param name="entityType">Type of entity</param>
+        /// <returns></returns>
+        public static string GetEntitySchemaName(Type entityType) 
+        {
+            return GetEntitySchemaName(entityType.Name);
+        }
+
+        /// <summary>
+        /// Get schema name for entity with the specified type name (<paramref name="entityTypeName"/>)
+        /// </summary>
+        /// <param name="entityTypeName">Type name of the entity</param>
+        /// <returns></returns>
+        public static string GetEntitySchemaName(string entityTypeName)
+        {
+            return entityTypeName.ToCamelCase();
+        }
+
+        public virtual Task<ISchema> GetOrDefaultAsync(string schemaName, string defaultSchemaName = null)
+        {
+            ISchema schema = null;
+            if (!schemaName.IsNullOrEmpty())
+            {
+                schema = CustomSchemas.ContainsKey(schemaName)
+                    ? CustomSchemas[schemaName]
+                    : GetEntitySchema(schemaName);
+            }
+            
+            if (schema == null)
+                schema = defaultSchemaName != null ? CustomSchemas[defaultSchemaName] : DefaultSchema;
+
+            return Task.FromResult(schema);
+        }
+
+        public void HandleEvent(EntityChangedEventData<EntityProperty> eventData)
+        {
+            if (eventData.Entity?.EntityConfig == null)
+                return;
+
+            EntitySchemasCache.Remove(GetEntitySchemaName(eventData.Entity.EntityConfig.ClassName));
         }
     }
 }
