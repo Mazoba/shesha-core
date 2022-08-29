b8b12043215261141fa8d47e0760cc4512addf89
ivan <ivan@boxfusion.co.za>
2022-08-29 15:27:52 +0300

    feat: restored generic export to Excel

--- src/Shesha.Application/DynamicEntities/EntitiesAppService.cs
+++ src/Shesha.Application/DynamicEntities/EntitiesAppService.cs
@@ -1,13 +1,21 @@
-﻿using Abp.Authorization;
+﻿using Abp.Application.Services.Dto;
+using Abp.Authorization;
 using Abp.Domain.Entities;
 using Abp.ObjectMapping;
+using GraphQL;
+using GraphQL.Execution;
 using Microsoft.AspNetCore.Mvc;
 using Shesha.Application.Services;
 using Shesha.Application.Services.Dto;
 using Shesha.Configuration.Runtime;
 using Shesha.Configuration.Runtime.Exceptions;
+using Shesha.Excel;
 using Shesha.Utilities;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Shesha.DynamicEntities
@@ -16,11 +24,14 @@ namespace Shesha.DynamicEntities
     public class EntitiesAppService: SheshaAppServiceBase
     {
         private readonly IEntityConfigurationStore _entityConfigStore;
+        private readonly IExcelUtility _excelUtility;
+        
         public IObjectMapper AutoMapper { get; set; }
 
-        public EntitiesAppService(IEntityConfigurationStore entityConfigStore)
+        public EntitiesAppService(IEntityConfigurationStore entityConfigStore, IExcelUtility excelUtility)
         {
             _entityConfigStore = entityConfigStore;
+            _excelUtility = excelUtility;
         }
 
         [HttpGet]
@@ -43,9 +54,6 @@ namespace Shesha.DynamicEntities
 
                 // parse id value to concrete type
                 var parsedId = Parser.ParseId(input.Id, entityConfig.EntityType);
-
-                // make generic type
-                //var invokerType = typeof(IEntityAppService<,>).MakeGenericType(entityConfig.EntityType, entityConfig.IdType);
 
                 var methodName = nameof(IEntityAppService<Entity<Int64>, Int64>.QueryAsync);
                 var method = appService.GetType().GetMethod(methodName);
@@ -101,6 +109,64 @@ namespace Shesha.DynamicEntities
             {
                 throw;                
             }
+        }
+
+        [HttpPost]
+        public async Task<FileStreamResult> ExportToExcel(ExportToExcelInput input, CancellationToken cancellationToken) 
+        {
+            try
+            {
+                var entityConfig = _entityConfigStore.Get(input.EntityType);
+                if (entityConfig == null)
+                    throw new EntityTypeNotFoundException(input.EntityType);
+
+                var appServiceType = entityConfig.ApplicationServiceType;
+
+                if (entityConfig.ApplicationServiceType == null)
+                    throw new NotSupportedException($"{nameof(GetAllAsync)} is not implemented for entity of type {entityConfig.EntityType.FullName}");
+
+                var appService = IocManager.Resolve(appServiceType) as IEntityAppService;
+                if (appService == null)
+                    throw new NotImplementedException($"{nameof(IEntityAppService)} is not implemented by type {entityConfig.ApplicationServiceType.FullName}");
+
+                var data = await appService.QueryAllAsync(input);
+                
+                var rows = ExtractGqlListData(data);
+
+                var stream = await _excelUtility.ReadToExcelStreamAsync(entityConfig.EntityType, rows, input.Columns, "Sheet1");
+                stream.Seek(0, SeekOrigin.Begin);
+
+                return new FileStreamResult(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        private IEnumerable<Dictionary<string, object>> ExtractGqlListData(IDynamicDataResult dataResult) 
+        {
+            var jsonData = (Microsoft.AspNetCore.Mvc.JsonResult)dataResult;
+            if (jsonData.Value is ExecutionResult executionResult && executionResult.Data is ExecutionNode executionNode)
+            {
+                var value = executionNode.ToValue();
+                if (executionNode is ObjectExecutionNode objectExecutionNode)
+                {
+                    if (objectExecutionNode.SubFields != null)
+                    {
+                        var root = objectExecutionNode.SubFields.FirstOrDefault(); // field itself e.g. '*List'
+                        var listResponse = (root as ObjectExecutionNode).SubFields;
+                        var itemsFieldName = StringHelper.ToCamelCase(nameof(PagedResultDto<EntityDto<Guid>>.Items));
+                        var itemsArray = listResponse.OfType<ArrayExecutionNode>().FirstOrDefault(f => f.Name == itemsFieldName);
+                        if (itemsArray != null)
+                        {
+                            var rows = itemsArray.Items.OfType<ObjectExecutionNode>();
+                            return rows.Select(row => row.ToValue() as Dictionary<string, object>);
+                        }
+                    }
+                }
+            }
+            return null;
         }
     }
 }
